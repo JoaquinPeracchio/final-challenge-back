6fd1d2f13882cdf5e479379a5810ed184bb9e105
lec89 <lucascorts@hotmail.com>
2022-10-04 11:16:45 +0200

    email and role added

--- models/User.js
+++ models/User.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     lastName: { type: String, required: true },
-    photo: { type: String },
+    photo: { type: String, required: true },
+    email: { type: String, required: true },
     password: [{ type: String, required: true }],
-    adress: { type: String, required: true },
+    address: { type: String, required: true },
     phone: { type: Number, required: true },
     sells: { type: Number },
     buys: { type: mongoose.Types.ObjectId, ref: 'products'},
@@ -14,7 +15,8 @@ const userSchema = new mongoose.Schema({
     from: [{ type: String, required: true }],
     verified: { type: Boolean, required: true },
     code: { type: String, required: true },
-    logged: { type: Boolean, required: true }
+    logged: { type: Boolean, required: true },
+    role: {type: String, required: true}
 });
 
 const User = mongoose.model('users', userSchema);
