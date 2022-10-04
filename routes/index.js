6c750daf65d8f0f203dca8b9d424b685097ed6ee
lec89 <lucascorts@hotmail.com>
2022-10-04 12:11:14 +0200

    products added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const commentRouter = require('./comments')
 const userRouter = require('./users')
+const productRouter = require('./products')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -10,5 +11,6 @@ router.get('/', function (req, res, next) {
 
 router.use('/comments', commentRouter)
 router.use('/users', userRouter)
+router.use('/products', productRouter)
 
 module.exports = router;
