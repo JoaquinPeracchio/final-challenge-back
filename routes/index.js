1c20df09b6492e695a117817fd5df0ac263b997b
LilAguh <aguezeochoa@gmail.com>
2022-10-04 05:27:29 -0300

    se agregan las rutas de comentarios a index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const commentRouter = require('./comments')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.use('/comments', commentRouter)
 module.exports = router;
