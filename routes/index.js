04c3b8c519ec986239297708eb0519addae623af
lec89 <lucascorts@hotmail.com>
2022-10-04 11:17:11 +0200

    userRouter added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const commentRouter = require('./comments')
+const userRouter = require('./users')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -8,4 +9,6 @@ router.get('/', function (req, res, next) {
 });
 
 router.use('/comments', commentRouter)
+router.use('/users', userRouter)
+
 module.exports = router;
