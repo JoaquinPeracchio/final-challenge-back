ff7974bcc85745b4636d22e766abe8ec71be8be1
lec89 <lucascorts@hotmail.com>
2022-10-04 12:10:05 +0200

    products added

---
+++ routes/products.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+// const { create, read, update, destroy } = require('')
+
+// router.post('/', create);
+// router.get('/', read);
+// router.patch('/', update);
+// router.delete('/', destroy);
+
+module.exports = router;
