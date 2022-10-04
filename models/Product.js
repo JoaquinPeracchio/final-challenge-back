77e65438b8830167017b1a083e3daccc4aca6388
lec89 <lucascorts@hotmail.com>
2022-10-04 22:13:03 +0200

    product modified

--- models/Product.js
+++ models/Product.js
@@ -2,8 +2,15 @@ const mongoose = require("mongoose")
 
 const productsSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    user: { type: mongoose.Types.ObjectId, ref: 'users'},
+    type: { type: String, required: true },
+    variety: { type: String, required: true },
+    quantitymin: { type: Number, required: true },
+    currentState: { type: String, required: true },
+    stock: { type: Number, required: true },
+    description: { type: String, required: true },
     price: { type: Number, required: true },
-    photo: {type: String, required: true},
+    photo: { type: String, required: true },
     likes: { type: Array, required: true }
 })
 
