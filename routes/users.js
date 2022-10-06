b837c91e415e36dece726588a8bfb9ce2b859d4d
jojope38 <peracchiojoaquin16@gmail.com>
2022-10-06 14:48:42 -0300

    added router to respective controllers

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const { read, signUp, signIn, signOut } = require('../controllers/usersController');
+const { read, signUp, signIn, signOut, sells, buys } = require('../controllers/usersController');
 
 router.get('/', read);
 router.post('/signup', signUp);
 router.post('/signin', signIn);
 router.post('/signout', signOut);
-
+router.post('/sell/:id', sells)
+router.post('/buy/:id',buys)
 module.exports = router;
