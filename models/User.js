2922330a9442b1fac40c7d739b7369a6548e1918
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-10-06 00:10:30 -0300

    user model modified

--- models/User.js
+++ models/User.js
@@ -4,17 +4,16 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     lastName: { type: String, required: true },
     photo: { type: String },
+    mail: { type: String, required: true },
     password: [{ type: String, required: true }],
     adress: { type: String, required: true },
     phone: { type: Number, required: true },
-    sells: { type: Number },
-    buys: { type: mongoose.Types.ObjectId, ref: 'products'},
     popularity: [{ type: Number }],
-    comment: [{ type: mongoose.Types.ObjectId, ref: 'comments' }],
     from: [{ type: String, required: true }],
-    verified: { type: Boolean, required: true },
-    code: { type: String, required: true },
-    logged: { type: Boolean, required: true }
+    verification: { type: Boolean, required: true },
+    uniqueString: { type: String, required: true },
+    logged: { type: Boolean, required: true },
+    role: { type: String, required: true }
 });
 
 const User = mongoose.model('users', userSchema);
