5dfa5de9e59373454209da6627e402995c240a90
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-10-06 00:10:15 -0300

    users controller created

---
+++ controllers/Users.js
@@ -0,0 +1,94 @@
+const User = require('../models/User.js');
+const crypto = require('crypto');
+const bcryptjs = require('bcryptjs');
+const { exists } = require('../models/User.js');
+
+const usersController = {
+
+    signUp: async (req, res) => {
+        let { name, lastName, photo, mail, password, adress, phone, sells, buys, popularity, comment, from, role } = req.body;
+        try {
+            let user = await User.findOne({ mail });
+            if (!user) {
+                let logged = false;
+                let verification = false;
+                let uniqueString = crypto.randomBytes(15).toString('hex');
+                if (from === 'form') {
+                    password = bcryptjs.hashSync(password, 10);
+                    user = await new User({ name, lastName, photo, mail, password: [password], adress, phone, popularity, role, from: [from], logged, verification, uniqueString }).save();
+                    // sendMail(mail, code);
+                    res.status(201).json({
+                        message: "user signed up from form",
+                        success: true
+                    });
+                } else { //GOOGLE
+                    password = bcryptjs.hashSync(password, 10);
+                    verified = true;
+                    user = await new User({ name, lastName, photo, mail, password: [password], adress, phone, popularity, role, from: [from], logged, verification, uniqueString }).save();
+                    res.status(201).json({
+                        message: "user signed up from " + from,
+                        success: true
+                    });
+                }
+            } else if (user.mail === mail) {
+                res.status(400).json({ message: 'User already exists', success: false });
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({ message: error, success: false });
+        }
+    },
+
+    signIn: async (req, res) => {
+        let { mail, password } = req.body;
+        try {
+            let user = await User.findOne({ mail });
+            if (!user) {
+                res.status(404).json({ message: `Please check your credentials`, success: false });
+            } else {
+                let checkPass = user.password.filter(passwordElem => bcryptjs.compareSync(password, passwordElem))
+                if (mail === user.mail && checkPass.length > 0) {
+                    user.logged = true;
+                    await user.save();
+                    res.status(200).json({ message: `Welcome ${user.name} ${user.lastName}`, success: true })
+                }
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({ message: error, success: false });
+        }
+    },
+
+    signOut: async (req, res) => {
+        let { mail } = req.body
+        try {
+            const user = await User.findOne({ mail: mail })
+            if (user) {
+                user.logged = false
+                await user.save()
+                res.status(200).json({
+                    succes: true,
+                    message: 'Session finished',
+                })
+            }
+        } catch (error) {
+            console.log(error)
+            res.status(400).json({ message: error, success: false })
+        }
+    },
+
+    read: async (req, res) => {
+        try {
+            let users = await User.find();
+            if (users) {
+                res.status(200).json({ message: 'Users found', response: users, success: true });
+            } else {
+                res.status(404).json({ message: 'Users not found', success: false });
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+module.exports = usersController
