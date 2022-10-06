cf6b5b6d2e0b707a85b6feb982900bcecc06fcd8
LilAguh <aguezeochoa@gmail.com>
2022-10-05 21:30:47 -0300

    se agrega la ruta para las respuestas

--- routes/comments.js
+++ routes/comments.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const { createComment, editComment, readComment, readCommentByID, deleteComment } = require('../controllers/commentsController')
+const { createComment, createResponse, readComment, readCommentByID, deleteComment } = require('../controllers/commentsController')
 
 router.post('/', createComment)
-router.put('/:id', editComment)
+router.put('/:id', createResponse)
 router.get('/product/:id', readComment)
 router.get('/:id', readCommentByID)
 router.delete('/product/:id', deleteComment)
