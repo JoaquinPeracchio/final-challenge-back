d3300d7b47db3dd93931259d7a3796691f7f9ebe
lec89 <lucascorts@hotmail.com>
2022-10-10 19:25:17 -0300

    get all added

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const { create, read, update, destroy } = require('../controllers/productsController')
+const { create, all, read, update, destroy } = require('../controllers/productsController')
 
 router.post('/', create);
+router.get('/', all)
 router.get('/:id', read);
 router.patch('/:id', update);
 router.delete('/:id', destroy);
