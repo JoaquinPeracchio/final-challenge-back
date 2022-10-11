0cb6bdbc3618fb222c3fcf7377ff88185be39d8b
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-10-11 12:46:50 -0300

    routes added

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const { read, signUp, signIn, signOut, sells, buys } = require('../controllers/usersController');
+const { read, signUp, signIn, signOut, sells, buys, emailVerificaiton } = require('../controllers/usersController');
 
 router.get('/', read);
 router.post('/signup', signUp);
 router.post('/signin', signIn);
 router.post('/signout', signOut);
-router.post('/sell/:id', sells)
-router.post('/buy/:id',buys)
+router.post('/sell/:id', sells);
+router.post('/buy/:id',buys);
+router.get('/verify/:uniqueString', emailVerificaiton);
 module.exports = router;
