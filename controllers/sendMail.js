617e47ce36152b73917eb720dbbbe3ce762a016b
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-10-11 12:45:57 -0300

    send mail controller created

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -1,55 +1,56 @@
 const nodemailer = require('nodemailer');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
-let {GOOGLE_ID, GOOGLE_SECRET, GOOGLE_URL, GOOGLE_REFRESH, GOOGLE_USER} = process.env;
+const { GOOGLE_ID, GOOGLE_SECRET, GOOGLE_REFRESH, GOOGLE_URL, GOOGLE_USER } = process.env
 
-const sendMail => async (mail, code){
+
+const sendMail = async (mail, code) => {
+
 	const client = new OAuth2(
 		GOOGLE_ID,
 		GOOGLE_SECRET,
-		GOOGLE_URL
+		GOOGLE_URL,
 	);
 
 	client.setCredentials({
 		refresh_token: GOOGLE_REFRESH
 	});
 
-	const accessToken = client.getAccessToken();
+	const accesToken = client.getAccessToken();
 
 	const transport = nodemailer.createTransport({
 		service: 'gmail',
 		auth: {
 			user: GOOGLE_USER,
 			type: 'OAuth2',
-			clientID: GOOGLE_ID,
+			clientId: GOOGLE_ID,
 			clientSecret: GOOGLE_SECRET,
-			refresh_token: GOOGLE_REFRESH,
-			accessToken: accessToken
+			refreshToken: GOOGLE_REFRESH,
+			accesToken: accesToken
 		},
-
 		tls: {
 			rejectUnauthorized: false
 		}
-	})
+	});
 
-	const mailOptions = {
+	const mailOpcions = {
 		from: GOOGLE_USER,
 		to: mail,
-		subject: 'Código de verificación de Terra Farms Market',
-		html: 
-				`<div>
-					<h1>Hola, ${mail}</h1>
-					<p>We need to verify your email account, please follow the link below</p>
-					<p></p>
-				</div>`
+		subject: 'Verification code for Terra Farms Market',
+		html: `
+        <div>
+			<h1>Hi!, ${mail}</h1>
+			<p>We need to verify your email account, please follow the link below</p>
+			<a href='http://localhost:4000/auth/verify/${code}'>Click to verify</a>
+        </div>
+    `
 	}
 
-	await transport.sendMail(mailOptions, (error, response) => {
-		if (error) {
-			console.log(error);
+	transport.sendMail(mailOpcions, (err, res) => {
+		if (err) {
+			console.log(err)
 		} else {
-			console.log('Mail sent correctly');
-			console.log(response);
+			console.log('Mail sent correctly')
 		}
 	})
 }
