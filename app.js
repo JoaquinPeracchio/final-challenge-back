8aae2db2dec59e89c26a87ce11a9db01658e18e4
Joaquin Peracchio <88782590+JoaquinPeracchio@users.noreply.github.com>
2022-10-13 00:20:47 -0300

    app , routes products

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 require('dotenv').config()
 require('./config/database')
 var createError = require('http-errors');
@@ -5,7 +6,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
